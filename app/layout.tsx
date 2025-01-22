92bd8d10274ff3aa7a3ad4fedfef227ab6d7a848
Alex Cho <alexcho@bytedance.com>
2025-01-22 10:56:51 -0800

    successfully deployed to github pages. need to fix img rendering issues next

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Alex Cho - Product Manager',
+  title: 'Alex Cho Portfolio',
   description: 'Product Manager Portfolio',
   icons: {
     icon: 'img/about.png',
