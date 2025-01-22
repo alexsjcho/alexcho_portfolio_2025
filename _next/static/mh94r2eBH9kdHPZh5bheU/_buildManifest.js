8f7e9ad8b6439db531bdbc6c5e2f31d42de7a940
Alex Cho <alexcho@bytedance.com>
2025-01-22 13:45:51 -0800

    Updates

---
+++ _next/static/mh94r2eBH9kdHPZh5bheU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/404":["static/chunks/pages/404-2863cde934f1568f.js"],"/500":["static/chunks/pages/500-f18e0b121e3ea46d.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/404","/500","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
